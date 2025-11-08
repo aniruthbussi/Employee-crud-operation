685dff6bfef4dfaa3b82708b2a9bcebd2fe73046
AJ <57801490+aniruthbussi@users.noreply.github.com>
2025-11-08 07:20:15 +0530

    Add files via upload

---
+++ employee/src/main/java/com/codeWithProject/employee/Repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.codeWithProject.employee.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.codeWithProject.employee.Entity.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+	
+}
